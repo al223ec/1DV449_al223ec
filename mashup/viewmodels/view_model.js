f56ee9dd9489177f8774ed665af83477070aced5
al223ec <al223ec@student.lnu.se>
2014-12-08 15:47:27 +0100

    Map style

--- mashup/viewmodels/view_model.js
+++ mashup/viewmodels/view_model.js
@@ -102,26 +102,31 @@ function getIcon(priority){
   //priority - Meddelandets prioritet (1 = Mycket allvarlig händelse, 2 = Stor händelse, 3 = Störning, 4 = Information, 5 = Mindre störning)
   switch(priority){
     case 1 : 
-      return 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'; 
+      return 'img/red-dot.png'; 
     case 2 : 
-      return 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png';
+      return 'img/orange-dot.png';
     case 3 : 
-      return 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'; 
+      return 'img/yellow-dot.png'; 
     case 4 : 
-      return 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'; 
+      return 'img/blue-dot.png'; 
     case 5 : 
-      return 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+      return 'img/green-dot.png';
     default : 
-      return 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png'; 
+      return 'img/purple-dot.png'; 
   } 
 }
+//http://maps.google.com/mapfiles/ms/icons/yellow-dot.png
 var map;
 function createMap(){    
   var mapOptions = {
       zoom: 6,
       center: new google.maps.LatLng(62.38, 17.3), //Svall
-      mapTypeId: 'terrain'
-  };
+
+      styles : [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]},{"featureType":"landscape","stylers":[{"color":"#f2e5d4"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}]
+      //styles : [{"featureType":"water","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":-78},{"lightness":67},{"visibility":"simplified"}]},{"featureType":"landscape","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"geometry","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"simplified"}]},{"featureType":"poi","elementType":"all","stylers":[{"hue":"#ffffff"},{"saturation":-100},{"lightness":100},{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"hue":"#e9ebed"},{"saturation":-90},{"lightness":-8},{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"hue":"#e9ebed"},{"saturation":10},{"lightness":69},{"visibility":"on"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"hue":"#2c2e33"},{"saturation":7},{"lightness":19},{"visibility":"on"}]},{"featureType":"road","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":31},{"visibility":"on"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"hue":"#bbc0c4"},{"saturation":-93},{"lightness":-2},{"visibility":"simplified"}]}]
+      //styles : [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"road.arterial","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#5f94ff"},{"lightness":26},{"gamma":5.86}]},{},{"featureType":"road.highway","stylers":[{"weight":0.6},{"saturation":-85},{"lightness":61}]},{"featureType":"road"},{},{"featureType":"landscape","stylers":[{"hue":"#0066ff"},{"saturation":74},{"lightness":100}]}]
+
+    };
   map = new google.maps.Map($('#map-canvas')[0], mapOptions);
 }
 
